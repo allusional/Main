ef42196010b8d4d7dc6a1f1710ae683b412c99ff
allusional <chenallen221@gmail.com>
2026-07-28 20:02:01 +0000

    Security: block open redirect, add security headers, patch deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { securityHeaders } from "./src/lib/security";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  async headers() {
+    return [{ source: "/:path*", headers: securityHeaders() }];
+  },
 };
 
 export default nextConfig;
